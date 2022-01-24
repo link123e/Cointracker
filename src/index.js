933cc2299cd5ccc3d199f64327e48f036f2901d4
Linklinklink123 <link123e>
2022-01-24 18:34:08 +0900

    adsfadsfsdafadsfasdfasdfadssadfasdfa

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { render } from "react-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router basename={process.env.PUBLIC_URL}>
+    <Switch>
+        <Route path="/">
+        <App />
+      </Route>
+    </Switch>
+  </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
